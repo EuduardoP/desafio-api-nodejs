f1056746ff3c8098e6e97ccde934d823f894f3e3
Eduardo Pires <dudupiresrosa@gmail.com>
2025-08-18 12:18:58 -0300

    ✨ GetCoursesRoute now have enrollments
    #

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -1,3 +1,4 @@
+import { and, asc, count, eq, ilike, type SQL } from 'drizzle-orm'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
 import { db } from '../db/client.ts'
@@ -10,6 +11,18 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
       schema: {
         tags: ['courses'],
         summary: 'Get all courses',
+        querystring: z.object({
+          search: z
+            .string()
+            .optional()
+            .describe('Search term for course titles'),
+          orderBy: z
+            .enum(['id', 'title'])
+            .optional()
+            .default('id')
+            .describe('Order by field'),
+          page: z.coerce.number().optional().default(1).describe('Page number'),
+        }),
         response: {
           200: z.object({
             courses: z
@@ -17,22 +30,49 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
                 z.object({
                   id: z.uuid(),
                   title: z.string(),
+                  enrollmentsCount: z
+                    .number()
+                    .describe('Number of enrollments'),
                 })
               )
               .describe('List of courses'),
+            total: z.number().describe('Total number of courses'),
           }),
         },
       },
     },
-    async (_, reply) => {
-      const result = await db
-        .select({
-          id: schema.courses.id,
-          title: schema.courses.title,
-        })
-        .from(schema.courses)
+    async (request, reply) => {
+      const { search, orderBy, page } = request.query
 
-      return reply.send({ courses: result })
+      const conditions: SQL[] | undefined = []
+
+      if (search) {
+        conditions.push(ilike(schema.courses.title, `%${search}%`))
+      }
+
+      const [result, total] = await Promise.all([
+        db
+          .select({
+            id: schema.courses.id,
+            title: schema.courses.title,
+            enrollmentsCount: count(schema.enrollments.id).as(
+              'enrollments_count'
+            ),
+          })
+          .from(schema.courses)
+          .leftJoin(
+            schema.enrollments,
+            eq(schema.enrollments.courseId, schema.courses.id)
+          )
+          .orderBy(asc(schema.courses[orderBy]))
+          .offset((page - 1) * 2)
+          .limit(10)
+          .where(and(...conditions))
+          .groupBy(schema.courses.id),
+        db.$count(schema.courses, and(...conditions)),
+      ])
+
+      return reply.send({ courses: result, total })
     }
   )
 }
